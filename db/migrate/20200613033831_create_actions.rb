cbcfa24fdac93ba012693c0b344db9fd68a77467
jasmosez <jas.schaffer@gmail.com>
2020-06-13 00:15:38 -0400

    tweak actions table

--- db/migrate/20200613033831_create_actions.rb
+++ db/migrate/20200613033831_create_actions.rb
@@ -2,9 +2,13 @@ class CreateActions < ActiveRecord::Migration[6.0]
   def change
     create_table :actions do |t|
       t.string :title
+      t.string :details
       t.integer :duration
       t.string :url
       t.string :subject
+      t.string :category
+      t.string :source
+
 
       t.timestamps
     end
