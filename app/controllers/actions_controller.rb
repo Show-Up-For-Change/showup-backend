82fbd7e88dd4bed349d14e71172faa89adff98d8
Elom Amouzou <elom1990@yahoo.com>
2020-06-13 17:15:53 -0600

    add show actions for actions controller

--- app/controllers/actions_controller.rb
+++ app/controllers/actions_controller.rb
@@ -2,8 +2,23 @@ class ActionsController < ApplicationController
 
   def index
     actions = Action.all
-    
+
     render json: actions.sample(3)
   end
-  
+
+  def show
+    user = User.find_by(fb: action_params[:id])
+    if user
+      render json: user.get_incomplete_actions.sample(3)
+    else
+      render json: {error: "Could not find user!", status: :not_acceptable}
+    end
+  end
+
+  private
+
+  def action_params
+    params.permit(:id)
+  end
+
 end
