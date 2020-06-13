8fada00d84a78c7a9c57fd3bf28073287cce7a82
Elom Amouzou <elom1990@yahoo.com>
2020-06-13 17:15:30 -0600

    add commitments and helper emthod for incomplete actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,24 @@
 class User < ApplicationRecord
 
   validates :fb, uniqueness: true
-  # has_many :commitments
+  has_many :commitments
   has_many :user_actions
   has_many :actions, through: :user_actions
 
   def completed_actions
     self.actions
+  end
+
+  def get_incomplete_actions
+    Action.all.select do |action|
+      !user_action_ids.include?(action.id)
+    end
+  end
+
+  def user_action_ids
+    self.user_actions.map do |user_action|
+      user_action.action_id
+    end
   end
 
 end
