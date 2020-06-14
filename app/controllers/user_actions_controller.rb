f25b727e5776315bcfb06d1850493ec7195d233f
jasmosez <jas.schaffer@gmail.com>
2020-06-13 20:41:27 -0400

    create destroy user-actions

--- app/controllers/user_actions_controller.rb
+++ app/controllers/user_actions_controller.rb
@@ -1,2 +1,26 @@
 class UserActionsController < ApplicationController
+
+  def create
+    user_action = UserAction.create(
+      duration: user_action_params[:duration],
+      comment: user_action_params[:comment],
+      action_id: user_action_params[:action_id],
+      user_id: User.find_by(fb: user_action_params[:user_id]).id,
+    )
+
+    render json: user_action
+
+  end
+
+  def destroy
+    user_action = UserAction.find(user_action_params[:id])
+    render json: user_action
+    UserAction.delete(user_action_params[:id])
+  end
+
+  private
+
+  def user_action_params
+    params.permit(:duration, :comment, :action_id, :user_id, :id)
+  end
 end
